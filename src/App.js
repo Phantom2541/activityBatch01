4af101f5ca631325ae41d11810901dbfaca35273
EmmAnneOwel <technowiz.ecaindec@gmail.com>
2025-01-12 10:04:20 +0800

    Added react router

--- src/App.js
+++ src/App.js
@@ -1,89 +1,17 @@
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Timer from "./components/Timer";
 
 export default function App() {
-  const [users, setUsers] = useState([
-      { id: 1, name: "John" },
-      { id: 2, name: "Doe" },
-      { id: 3, name: "Smith" },
-      { id: 4, name: "Mark" },
-      { id: 5, name: "Meluin" },
-    ]),
-    [user, setUser] = useState({});
-
-  // Delete Function
-  const deleteFxn = (_id) => setUsers(users.filter(({ id }) => id !== _id));
-
-  // Edit Function
-  const activeUser = (_id) => setUser(users.find(({ id }) => id === _id));
-  const updateUsers = (_user) => {
-    setUsers([...users.filter(({ id }) => id !== _user.id), _user]);
-    clearUser();
-  };
-
-  const clearUser = () => setUser({ id: users.length + 1, name: "" });
-
   return (
-    <div>
-      <h1>User users</h1>
-      <h2>Hello</h2>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(({ id, name }) => {
-              return (
-                <tr>
-                  <td>{id}</td>
-                  <td>{name}</td>
-                  <td>
-                    <button
-                      type="button"
-                      title="Edit"
-                      onClick={() => activeUser(id)}
-                    >
-                      Edit
-                    </button>
-                    <button
-                      type="button"
-                      title="Delete"
-                      onClick={() => deleteFxn(id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-      <hr />
-      <div>
-        <h3>User Form</h3>
-        <form>
-          <button type="button" onClick={clearUser}>
-            Create New
-          </button>
-          <br />
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={user.name}
-              onChange={(e) => setUser({ ...user, name: e.target.value })}
-            />
-          </label>
-          <button type="button" onClick={() => updateUsers(user)}>
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="timer" element={<Timer/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+export default function Home(){
+    const [users, setUsers] = useState([
+        { id: 1, name: "John" },
+        { id: 2, name: "Doe" },
+        { id: 3, name: "Smith" },
+        { id: 4, name: "Mark" },
+        { id: 5, name: "Meluin" },
+      ]),
+      [user, setUser] = useState({});
+  
+    // Delete Function
+    const deleteFxn = (_id) => setUsers(users.filter(({ id }) => id !== _id));
+  
+    // Edit Function
+    const activeUser = (_id) => setUser(users.find(({ id }) => id === _id));
+    const updateUsers = (_user) => {
+      setUsers([...users.filter(({ id }) => id !== _user.id), _user]);
+      clearUser();
+    };
+  
+    const clearUser = () => setUser({ id: users.length + 1, name: "" });
+  
+    return (
+        <div>
+      <h1>User users</h1>
+      <h2>Hello</h2>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(({ id, name }) => {
+              return (
+                <tr>
+                  <td>{id}</td>
+                  <td>{name}</td>
+                  <td>
+                    <button
+                      type="button"
+                      title="Edit"
+                      onClick={() => activeUser(id)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      type="button"
+                      title="Delete"
+                      onClick={() => deleteFxn(id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <hr />
+      <div>
+        <h3>User Form</h3>
+        <form>
+          <button type="button" onClick={clearUser}>
+            Create New
+          </button>
+          <br />
+          <label>
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={user.name}
+              onChange={(e) => setUser({ ...user, name: e.target.value })}
+            />
+          </label>
+          <button type="button" onClick={() => updateUsers(user)}>
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+    )
+}
